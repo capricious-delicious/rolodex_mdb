37296b4da679d8791df03ad8349b62718b7bd7b6
dannyeldridge <danieljameseldridge@Daniels-MacBook-Pro.local>
2020-05-03 14:44:12 -0700

    prepare for deply

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const connectDB = require('./config/db');
+const path = require('path');
 
 // Initalize the app.
 const app = express();
@@ -8,14 +9,19 @@ connectDB();
 // Initialize the middleware
 app.use(express.json({ extended: false }));
 
-app.get('/', (req, res) => {
-  res.send('API Running!');
-});
-
 // Define the routes
 app.use('/api/auth', require('./routes/api/auth'));
 app.use('/api/users', require('./routes/api/users'));
 app.use('/api/contacts', require('./routes/api/contacts'));
+
+// Serve static assets in production
+if (process.env.NODE_ENV === 'production') {
+  // Set static folder
+  app.use(express.static('client/build'));
+  app.get('*', (req, res) => {
+    res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
+  });
+}
 
 const PORT = process.env.PORT || 5000;
 
